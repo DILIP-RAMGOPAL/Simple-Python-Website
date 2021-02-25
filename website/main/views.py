242e25d1dde86457b670ecadf6e3fdf3745e55b5
Dilip <dilip@genskill.com>
2021-02-26 03:16:57 +0530

    added date to timezone converter

--- website/main/views.py
+++ website/main/views.py
@@ -31,22 +31,24 @@ def convert_timezone(request):
     ctxt = {"timezone": pytz.all_timezones}
     if request.method == 'POST':
         time = request.POST.get("time")
+        date = request.POST.get("date")
         if time:
             tz1 = request.POST.get("tz1")
             tz2 = request.POST.get("tz2")
-            time2 = convert_datetime_timezone(time, tz1, tz2)
-            ctxt["time"] = time2
+            time = time + " " + date
+            time = convert_datetime_timezone(time, tz1, tz2)
+            ctxt["time"] = time
     return render(request, "timezone.html", ctxt)
 
 
 def convert_datetime_timezone(dt, tz1, tz2):
     tz1 = pytz.timezone(tz1)
     tz2 = pytz.timezone(tz2)
-    dt = datetime.datetime.strptime(dt, "%H:%M")
+    dt = datetime.datetime.strptime(dt, "%H:%M %Y-%m-%d")
     dt = dt.replace(year=2021)
     dt = tz1.localize(dt)
     dt = dt.astimezone(tz2)
-    dt = dt.strftime("%H:%M")
+    dt = dt.strftime("%H:%M %Y-%m-%d")
     return dt
 
 
